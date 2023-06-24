792a19f26f98f0e3df752078a942f75657defb47
Aaron_Gp <1791615002@qq.com>
2023-06-24 21:17:49 +0800

    fix four more times

--- pages/index.vue
+++ pages/index.vue
@@ -3,12 +3,15 @@
         <Header />
     </header>
     <main class="main space-y-10 mb-40">
-        <Gradient class=" -z-10"/>
-        <button @click="{console.log(instance);instance>0?pass:getChild();instance++;}" class=" border border-slate-800 rounded">click me</button>
+        <Gradient class=" -z-10" />
+        <button @click="{ console.log(instance); instance > 0 ? pass : getChild(); instance++; }"
+            class=" border border-slate-800 rounded">click me</button>
         <!-- <n-button @click="{console.log(instance);instance>0?pass:getChild();instance++;}">
             点它1
         </n-button> -->
-        <Game ref="game" />
+        <ClientOnly>
+            <Game ref="game" />
+        </ClientOnly>
         <!-- <div class="" id="home">
             <Gradient />
             <h1>Hello Nuxt 3</h1>
